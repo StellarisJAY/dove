a7f1fb641f064bbbf81edd5e0ff96ccbfad81f77
StellarisJAY <760409558@qq.com>
2022-01-08 15:59:09 +0800

    add basic remoting methods

---
+++ src/main/java/com/jay/dove/transport/InvokeContext.java
@@ -0,0 +1,16 @@
+package com.jay.dove.transport;
+
+import java.net.InetSocketAddress;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/01/07 19:31
+ */
+public class InvokeContext {
+    private InetSocketAddress remoteAddress;
+
+}
