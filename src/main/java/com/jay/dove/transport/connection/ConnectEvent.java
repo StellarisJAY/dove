968536108f33c074c0b734ee53ca1af0dc57966a
StellarisJAY <760409558@qq.com>
2022-01-10 15:53:33 +0800

    add connection event handler

---
+++ src/main/java/com/jay/dove/transport/connection/ConnectEvent.java
@@ -0,0 +1,19 @@
+package com.jay.dove.transport.connection;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/01/10 11:55
+ */
+public enum ConnectEvent {
+    /**
+     * connect
+     */
+    CONNECT,
+    CONNECT_FAIL,
+    EXCEPTION,
+    CLOSE;
+}
