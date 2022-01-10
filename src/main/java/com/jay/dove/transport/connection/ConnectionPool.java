143db46cf99faea74b6d9c04653dd5b2ca075b51
StellarisJAY <760409558@qq.com>
2022-01-10 15:54:33 +0800

    update connection pool warm up

--- src/main/java/com/jay/dove/transport/connection/ConnectionPool.java
+++ src/main/java/com/jay/dove/transport/connection/ConnectionPool.java
@@ -7,7 +7,6 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -26,29 +25,23 @@ public class ConnectionPool {
      * connections list
      */
     private final CopyOnWriteArrayList<Connection> connections;
-    private final int limit;
     /**
      * select strategy
      */
     private final ConnectionSelectStrategy selectStrategy;
 
     private final ConnectionFactory connectionFactory;
-
-
-    private final Semaphore semaphore;
     /**
      * whether this pool is warming up.
      * If this pool is warming up, other threads can't add new Connection into it.
      */
     private final AtomicBoolean warmingUp = new AtomicBoolean(false);
 
-    public ConnectionPool(InetSocketAddress address, ConnectionFactory connectionFactory, ConnectionSelectStrategy selectStrategy, int limit) {
+    public ConnectionPool(InetSocketAddress address, ConnectionFactory connectionFactory, ConnectionSelectStrategy selectStrategy) {
         this.connectionFactory = connectionFactory;
         this.selectStrategy = selectStrategy;
-        this.limit = limit;
         connections = new CopyOnWriteArrayList<>();
         this.address = address;
-        semaphore = new Semaphore(limit);
     }
 
     public Connection getConnection(){
@@ -57,57 +50,61 @@ public class ConnectionPool {
     }
 
     public Connection createAndGetConnection(int timeout) throws Exception {
-        // check if it's able to create a connection now
-        if(!warmingUp.get() && semaphore.tryAcquire()){
-            // pool is warming up or full
-            Connection connection = connectionFactory.create(address, timeout);
-            connections.add(connection);
-            return connection;
-        }else{
-            // wait for a connection
-            while(true){
-                if(this.connections.size() > 0){
-                    return getConnection();
-                }
-            }
-        }
+        Connection connection = connectionFactory.create(address, timeout);
+        connections.add(connection);
+        return connection;
     }
 
     /**
-     * warm up a connection pool using an executor
+     * heal a connection pool using an executor
      * @param executor {@link ExecutorService}
      * @param timeout connect timeout
      */
-    public void warmUpConnectionPool(ExecutorService executor, int timeout) {
-        warmingUp.set(true);
+    public void healConnectionPool(ExecutorService executor, int expectedCount, int syncCreate, int timeout)throws Exception {
+        int currentSize = connections.size();
+
+        if(currentSize > expectedCount){
+            return;
+        }
+        if(syncCreate > 0){
+            for(int i = 0; i < syncCreate; i++){
+                Connection connection = connectionFactory.create(address, timeout);
+                connections.add(connection);
+            }
+        }
+
+        markAsyncWarmUpStart();
         Runnable warmUpTask = ()->{
             long startTime = System.currentTimeMillis();
             int retryTimes = 0;
             // max retry times
             int maxRetryTimes = Configs.connectMaxRetry();
-            for (int cur = connections.size(); cur < limit; cur++){
+            for (int cur = connections.size(); cur < expectedCount; cur++){
                 try{
-                    // acquire a semaphore, check whether the pool is full
-                    if(semaphore.tryAcquire()){
-                        Connection connection = connectionFactory.create(address, timeout);
-                        connections.add(connection);
-                    }
+                    Connection connection = connectionFactory.create(address, timeout);
+                    connections.add(connection);
                 }catch (Exception e){
-                    // retry and check retry times
-                    cur --;
-                    retryTimes++;
-                    if(retryTimes == maxRetryTimes){
-                        log.warn("connection pool warm up failed, target address {}", address);
-                        break;
-                    }
+                    log.error("connection warm up failed", e);
+                    break;
                 }
             }
             log.info("connection pool warm up for {} finished, time used: {}ms", address, (System.currentTimeMillis() - startTime));
             // finish warming up
-            warmingUp.set(false);
+            markAsyncWarmUpDone();
         };
         executor.submit(warmUpTask);
     }
 
+    private void markAsyncWarmUpStart(){
+        for(;;){
+            if(warmingUp.compareAndSet(false, true)){
+                break;
+            }
+        }
+    }
+
+    private void markAsyncWarmUpDone(){
+        warmingUp.set(false);
+    }
 
 }
