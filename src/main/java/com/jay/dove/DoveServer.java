774b84fd423896c830e2fc7cc18ec6de5eeb8bd4
StellarisJAY <760409558@qq.com>
2022-01-16 11:27:43 +0800

    add Server Send methods

--- src/main/java/com/jay/dove/DoveServer.java
+++ src/main/java/com/jay/dove/DoveServer.java
@@ -2,11 +2,15 @@ package com.jay.dove;
 
 import com.jay.dove.common.AbstractLifeCycle;
 import com.jay.dove.config.Configs;
+import com.jay.dove.transport.BaseRemoting;
 import com.jay.dove.transport.HeartBeatHandler;
 import com.jay.dove.transport.Url;
+import com.jay.dove.transport.callback.InvokeCallback;
+import com.jay.dove.transport.callback.InvokeFuture;
 import com.jay.dove.transport.codec.Codec;
 import com.jay.dove.transport.command.CommandChannelHandler;
 import com.jay.dove.transport.command.DefaultResponseHandler;
+import com.jay.dove.transport.command.RemotingCommand;
 import com.jay.dove.transport.connection.ConnectEventHandler;
 import com.jay.dove.transport.connection.Connection;
 import com.jay.dove.transport.connection.ConnectionManager;
@@ -16,6 +20,7 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.Attribute;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
@@ -47,7 +52,12 @@ public class DoveServer extends AbstractLifeCycle {
      */
     private ConnectEventHandler connectEventHandler;
 
+    /**
+     * server port
+     */
     private final int port;
+
+    private BaseRemoting baseRemoting;
 
     public DoveServer(Codec codec, int port) {
         this.bootstrap = new ServerBootstrap();
@@ -138,4 +148,71 @@ public class DoveServer extends AbstractLifeCycle {
         this.boss.shutdownGracefully();
         this.worker.shutdownGracefully();
     }
+
+    /**
+     * send a one-way request to target url.
+     * This method requires the server connection manager enabled.
+     * @param url {@link Url}
+     * @param command {@link RemotingCommand}
+     */
+    public void sendOneway(Url url, RemotingCommand command){
+        if(Configs.serverManageConnection()){
+            Connection connection = connectionManager.getConnection(url);
+            baseRemoting.sendOneway(connection, command);
+        }else{
+            throw new RuntimeException("server connection manager not available, can't send to target url");
+        }
+    }
+
+    /**
+     * send a one-way request to target channel
+     * @param channel {@link Channel}
+     * @param command {@link RemotingCommand}
+     */
+    public void sendOneway(Channel channel, RemotingCommand command){
+        Attribute<Connection> attr = channel.attr(Connection.CONNECTION);
+        Connection connection;
+        if(attr != null && (connection = attr.get()) != null){
+            baseRemoting.sendOneway(connection, command);
+        }else{
+            channel.writeAndFlush(command).addListener((ChannelFutureListener)listener->{
+                if(!listener.isSuccess()){
+                    log.error("send oneway failed, command: {}, target: {}", command, channel.remoteAddress());
+                }
+            });
+        }
+    }
+
+    /**
+     * send a request to target channel and await response synchronously.
+     * @param channel {@link Channel}
+     * @param command {@link RemotingCommand}
+     * @param callback {@link InvokeCallback}
+     * @return {@link RemotingCommand}
+     * @throws InterruptedException await response interrupted
+     */
+    public RemotingCommand sendSync(Channel channel, RemotingCommand command, InvokeCallback callback) throws InterruptedException {
+        InvokeFuture future = sendFuture(channel, command, callback);
+        return future.awaitResponse();
+    }
+
+    /**
+     * send a request to target channel asynchronously with future
+     * @param channel {@link Channel}
+     * @param command {@link RemotingCommand}
+     * @param callback {@link InvokeCallback}
+     * @return {@link InvokeCallback}
+     */
+    public InvokeFuture sendFuture(Channel channel, RemotingCommand command, InvokeCallback callback){
+        Attribute<Connection> attr = channel.attr(Connection.CONNECTION);
+        Connection connection;
+        if(attr != null && (connection = attr.get()) != null){
+            return baseRemoting.sendFuture(connection, command, callback);
+        }else{
+            throw new RuntimeException("This channel is not bind to a Connection instance. Can't send command with future.");
+        }
+    }
+
+
+
 }
